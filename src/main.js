d0115eead80706bb8ee3d1dc94ee36bdf7dbc61c
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-09 16:06:12 +0530

    gsap.set use

--- src/main.js
+++ src/main.js
@@ -3,19 +3,26 @@ import { gsap } from "gsap";
 // gsap.to(".box", { x: 900, duration: 2 });
 // const box = document.querySelectorAll(".box")
 
-gsap.fromTo(
-  [".box"],
-  {
-    y: 500,
-    backgroundColor: "blue",
-    duration: 8,
+// gsap.fromTo(
+//   [".box"],
+//   {
+//     y: 500,
+//     backgroundColor: "blue",
+//     duration: 8,
 
-  },
-  {
-    x: 500, //move 300px right
-    rotation: 360, // spin a full turn
-    duration: 2, //over 2 seconds
-    ease: "power2.out",
-    backgroundColor: "red",
-  },
-);
+//   },
+//   {
+//     x: 500, //move 300px right
+//     rotation: 360, // spin a full turn
+//     duration: 2, //over 2 seconds
+//     ease: "power2.out",
+//     backgroundColor: "red",
+//   },
+// );
+
+// gsap.set(".box", {
+//   x: 200,
+//   y: 200,
+//   backgroundColor: "pink",
+//   opacity: 0.1,
+// });
